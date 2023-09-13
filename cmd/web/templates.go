4019a96731ef8ca32ed0d3125f22372dd4e6f66a
Pannio <1821299140@qq.com>
2023-09-13 18:19:17 +0800

    让代码更具普适性 解析 ./ui/html/pages/partials/ 下的任何文件

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -24,17 +24,22 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		// 从完整文件路径中提取文件名（如 "home.tmpl"）并将其赋值给名称变量
 		name := filepath.Base(page)
-		// 创建一个片段，其中包含基本模板、任何部分模板和页面的文件路径。
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/pages/partials/nav.tmpl",
-			page,
-		}
-		// 将文件解析为模板集
-		ts, err := template.ParseFiles(files...)
+		// 将基本模板文件解析为模板集
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
+		// 调用此模板集 ts 上的 ParseGlob() 来添加任何 partials 页面。
+		ts, err = ts.ParseGlob("./ui/html/pages/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+		// 在此模板集 ts 上调用 ParseFiles() 添加页面模板
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
 		// 使用页面名称（如 "home.tmpl"）作为关键字，将模板集添加到map中。
 		cache[name] = ts
 	}
