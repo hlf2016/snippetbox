7aeb62db234be138e2103b5f5a14881265f37ced
Pannio <1821299140@qq.com>
2023-09-14 16:28:46 +0800

    自定义 github.com/julienschmidt/httprouter 404 报错提示 统一体验

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,6 +8,12 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	// 创建一个封装 notFound() 辅助函数的处理函数，然后将其指定为 404 Not Found 响应的自定义处理函数。
+	// 您还可以通过设置 router.MethodNotAllowed 来为 405 Method Not Allowed 响应设置自定义处理程序。
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 	// 当该处理程序接收到一个请求时，它会删除 URL 路径中的前导斜线，然后在 ./ui/static 目录中搜索相应的文件发送给用户。
 	// 因此，为了使该处理程序正常工作，我们必须在将 URL 路径传递给 http.FileServer 之前，去掉 URL 路径中以"/static "开头的斜线。
 	// 否则，它将寻找一个不存在的文件，用户将收到未找到的 404 页面响应。幸运的是，Go 包含了一个 http.StripPrefix() 助手，专门用于完成这项任务。
