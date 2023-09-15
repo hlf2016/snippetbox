169dfd5fcde19db42f9c72e5c8b42372e4d67c64
Pannio <1821299140@qq.com>
2023-09-15 14:33:26 +0800

    创建 snippet 表单错误回显 旧值填充

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -7,10 +7,21 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
+// 定义一个 snippetCreateForm 结构，用于表示表单数据和表单字段的验证错误。
+// 请注意，所有结构字段都是特意导出的（即以大写字母开头）。这是因为结构字段必须导出，才能在渲染模板时被 html/template 包读取
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// 因为Httprouter与“/”路径完全匹配，所以我们现在可以从此处理程序中删除对r.URL.Path！=“/”的手动检查。
+	// 因为httprouter与“/”路径完全匹配，所以我们现在可以从此处理程序中删除对r.URL.Path！=“/”的手动检查。
 	//if r.URL.Path != "/" {
 	//	app.notFound(w)
 	//	return
@@ -62,10 +73,16 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+	data.Form = snippetCreateForm{
+		Expires: 365,
+	}
 	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// 将请求正文大小限制为 4096 字节 如果超出大小 那么 r.ParseForm() 将会报错
+	//r.Body = http.MaxBytesReader(w, r.Body, 4096)
+
 	// 首先，我们调用 r.ParseForm()，将 POST 请求体中的任何数据添加到 r.PostForm 映射中。
 	// 这也同样适用于 PUT 和 PATCH 请求。如果出现任何错误，我们将使用 app.ClientError() 助手向用户发送 400 Bad Request 响应
 	err := r.ParseForm()
@@ -73,9 +90,6 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	// 使用 r.PostForm.Get() 方法从 r.PostForm 地图中获取标题和内容
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
 
 	// r.PostForm.Get() 方法总是以 *string* 形式返回表单数据。然而，我们希望过期值是一个数字，
 	// 并希望在 Go 代码中将其表示为整数。因此，我们需要使用 strconv.Atoi() 手动将表单数据转换为整数，如果转换失败，我们将发送 400 Bad Request 响应
@@ -85,7 +99,40 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+
+	// 处理 多值字段 如 多选
+	//for i, item := range r.PostForm["items"] {
+	//	fmt.Fprintf(w, "%d: Item %s\n", i, item)
+	//}
+
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 { // 当我们检查title字段的长度时，我们使用的是 utf8.RuneCountInString() 函数，而不是 Go 的 len() 函数。这是因为我们要计算的是标题中的字符数，而不是字节数。为了说明两者的区别，字符串 "Zoë "有 3 个字符，但长度为 4 字节，因为有元音 ë 字符。
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Title, form.Content, expires)
 	if err != nil {
 		app.serverError(w, err)
 		return
