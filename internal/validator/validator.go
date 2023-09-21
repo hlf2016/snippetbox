30899058d51f82880809c85735f4a51086baa27a
Pannio <1821299140@qq.com>
2023-09-21 23:02:48 +0800

    使用 泛型 修改 validator.go 中的 PermittedInt 为 PermittedValue 类型 支持更多类型校验

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -53,9 +53,9 @@ func MinChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) >= n
 }
 
-func PermittedInt(value int, permittedValue ...int) bool {
-	for i := range permittedValue {
-		if value == permittedValue[i] {
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
 			return true
 		}
 	}
