8d06a47d19a47ef4b025ae48685ced8c2665753b
Pannio <1821299140@qq.com>
2023-09-22 22:40:25 +0800

    e2e test

--- cmd/web/handler_test.go
+++ cmd/web/handler_test.go
@@ -4,38 +4,34 @@ import (
 	"bytes"
 	"github.com/hlf2016/snippetbox/internal/assert"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	// 它本质上是 http.ResponseWriter 的一种实现，用于记录响应状态代码、标题和正文，而不是将其实际写入 HTTP 连接。
-	rr := httptest.NewRecorder()
+	//创建应用程序结构体的新实例。目前，该结构只包含几个模拟日志记录器（它们会丢弃写入其中的任何内容）。
+	app := &application{
+		errorLogger: log.New(io.Discard, "", 0),
+		infoLogger:  log.New(io.Discard, "", 0),
+	}
 
-	// 初始化一个新的虚拟http.Request
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	// 然后，我们使用 httptest.NewTLSServer() 函数创建一个新的测试服务器，并将 app.routes() 方法返回的值作为服务器的处理程序。
+	// 这样就启动了一个 HTTPS 服务器，在测试期间监听本地计算机随机选择的端口。请注意，我们推迟了对 ts.Close() 的调用，以便在测试结束时关闭服务器
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+	// 测试服务器正在侦听的网络地址包含在ts.URL字段中。
+	// 我们可以将其与ts.Client().Get()方法一起使用，以对测试服务器发出GET ping请求。这将返回一个包含响应的HTTP.Response结构。
+	rs, err := ts.Client().Get(ts.URL + "/ping")
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// 调用ping处理程序函数，传入httptest.ResponseRecorder和Http.Request。
-	ping(rr, r)
-
-	// 调用 http.ResponseRecorder 的 Result() 方法，获取 ping 处理程序生成的 http.Response。
-	rs := rr.Result()
-
 	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	// 我们可以检查 ping 处理程序写入的响应体是否等于 "OK"。
-	defer rs.Body.Close()
 	body, err := io.ReadAll(rs.Body)
 	if err != nil {
-		// 我们在几个地方使用了 t.Fatal() 函数来处理测试代码中出现意外错误的情况。
-		// 调用时，t.Fatal() 会将测试标记为失败，记录错误，然后完全停止当前测试（或子测试）的执行。
 		t.Fatal(err)
 	}
 	bytes.TrimSpace(body)
-
 	assert.Equal(t, string(body), "OK")
 }
